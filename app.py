bdc09854e0861280eb30e50e3e8f720bbb94887a
Ekaterina_Kugot <kali.kugot@yandex.ru>
2022-11-17 18:38:34 +0800

    combining flask and html

--- app.py
+++ app.py
@@ -3,60 +3,64 @@ from flask import Flask, flash, request, redirect, url_for, render_template
 from markupsafe import escape
 from werkzeug.exceptions import HTTPException
 
+UPLOAD_FOLDER = 'static/images'
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 #печать в консоль
 def log(text):
     app.logger.info(text)
 
-#url_for берёт ссылку, которая написана над функцией user_form
-@app.route("/")
-def main_page():
-    log("main page")
-    return '<a href="'+url_for('user_form')+'">Нажми на меня!</a>'
+def allowed_file(filename): #проверка картинка ли загружена
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-# get метод срабатывает, когда мы просто заходим на страницу
-@app.get("/users")
-def user_form():
-    return render_template('user_form.html')
+@app.route('/', methods=['GET', 'POST']) #основная стр
+def index():
+    if request.method == 'POST':
+        message = request.form.get('message') #получение текста
+        f = request.files['img']
+        if f and allowed_file(f.filename): # сохранение картинки
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+    return render_template('index.html')
 
-# post метод срабатывает, когда отправляется форма
-@app.post("/users")
-def user_page():
-    #здесь все переданные post аргументы
-    log(request.form)
-    
-    n = request.form["name"]
-    l = request.form["lastname"]
-    return render_template('user_page.html',name=n,lastname=l)
+@app.route('/userlist', methods=['GET', 'POST']) #стр userlist
+def userlist():
+    if request.method == 'POST':
+        nickname = request.form.get('nickname')#получение имени пользователя
+        password = request.form.get('password')#получение пароля
+    return render_template('userlist.html')
 
-#интересная передача параметра
-@app.route('/<text>')
-def show_text(text):
-    return "Your text is: "+text
     
 @app.errorhandler(HTTPException)
 def handle_exception(e):
     return render_template('error_page.html',code=e.code,name=e.name), e.code
-    
+
 #debug=True, иначе log не будет работать
 if __name__ == "__main__":
     # app.secret_key = os.urandom(24)
-    app.run(host="0.0.0.0",debug=True)
-#код ниже не работать не будет, не добавлять
+    app.run(host="0.0.0.0", debug=True)
 
-    
-#старая попытка (рабочая)   
-# def get_user_page(name, lastname):
-    # return render_template('user_page.html',name=name,lastname=lastname)
 
-# @app.route("/users", methods=['GET', 'POST'])
-# def users_page():
-    # if request.method == 'POST':
-        # log(request.form)
-        # log(request.args.get)
-        # a = request.form
-        # return get_user_page(a["name"],a["lastname"])
-    # return render_template('user_form.html')
-    
+#url_for берёт ссылку, которая написана над функцией user_form
+#@app.route("/")
+#def main_page():
+#    log("main page")
+#    return '<a href="'+url_for('user_form')+'">Нажми на меня!</a>'
+
+# post метод срабатывает, когда отправляется форма
+#@app.post("/users")
+#def user_page():
+    #здесь все переданные post аргументы
+#    log(request.form)    
+#    n = request.form["name"]
+#    l = request.form["lastname"]
+#    return render_template('user_page.html',name=n,lastname=l)
+
+#интересная передача параметра
+#@app.route('/<text>')
+#def show_text(text):
+#    return "Your text is: "+text
---
+++ static/js/script.js
@@ -0,0 +1,69 @@
+
+function deleteAllCookies() {
+  var cookies = document.cookie.split("; ");
+  for (var c = 0; c < cookies.length; c++) {
+      var d = window.location.hostname.split(".");
+      while (d.length > 0) {
+          var cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
+          var p = location.pathname.split('/');
+          document.cookie = cookieBase + '/';
+          while (p.length > 0) {
+              document.cookie = cookieBase + p.join('/');
+              p.pop();
+          };
+          d.shift();
+      }
+  }
+}
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+if (document.cookie.length == 0) {
+  document.cookie='user=000000'
+  let user = '000000'
+}
+
+if (location.href.match(/[\d\w-]+\.\w+$/)=='userlist.HTML'){
+  if (getCookie('user')!='000000' && user!='000000') for (let i=0; i<document.getElementsByTagName('main')[0].childElementCount; i++){
+    document.getElementsByClassName('newPost')[0].style.display='none'
+    document.getElementsByTagName('main')[0].children[i].lastElementChild.style.display='none'
+  }
+  else
+    for (let i=0; i<document.getElementsByTagName('main')[0].childElementCount; i++) document.getElementsByTagName('main')[0].children[i].lastElementChild.removeAttribute('style')
+}
+
+function divval(event){
+  let pass = prompt('Введите пароль:')
+  for (let i=0; i<document.getElementsByTagName('main')[0].childElementCount; i++) document.getElementsByTagName('main')[0].children[i].removeAttribute('style')
+  deleteAllCookies()
+  document.cookie='user='+ encodeURIComponent(event.firstElementChild.innerHTML)
+  user = event.firstElementChild.innerHTML
+  if (getCookie('user')!='000000' && user!='000000'){
+    document.getElementsByClassName('newPost')[0].style.display='none'
+    for (let i=0; i<document.getElementsByTagName('main')[0].childElementCount; i++) document.getElementsByTagName('main')[0].children[i].lastElementChild.style.display='none'
+  }
+  else{
+    document.getElementsByClassName('newPost')[0].removeAttribute('style')
+    for (let i=0; i<document.getElementsByTagName('main')[0].childElementCount; i++) document.getElementsByTagName('main')[0].children[i].lastElementChild.removeAttribute('style')
+  }
+  event.style.boxShadow = "0 0 4px 3px #bac6d2"
+}
+
+//fetch('http://numbersapi.com/24')
+//.then(promiseResult => { return promiseResult.text() })
+//.then(function(html) {
+  //var parser = new DOMParser();
+//  var doc = parser.parseFromString(html, "text/html");
+  //document.body.getElementsByTagName('main').appendChild(doc.documentElement)
+//})
+
+function dis_like(event){
+  if (event.style.backgroundColor == 'rgb(186, 198, 210)'){
+    if (event.classList.contains('dislike')) event.style.backgroundColor = '#d0a09f'
+    else event.style.backgroundColor = '#96bf93'
+  }
+  else event.style.backgroundColor = '#bac6d2'
+}
